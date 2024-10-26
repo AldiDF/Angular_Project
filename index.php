b39eea826dfa32606cd006a439d952c7d062cc62
AldiDF <aldidaffaarisyi@gmail.com>
2024-10-26 15:43:28 +0800

    update

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home - Top Up Store</title>
+    <title>Beranda</title>
     <link rel="stylesheet" href="styles/main.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer">
 </head>
 <body>
     <div class="off-screen-menu">
@@ -15,8 +16,20 @@
             <li>Upload</li>
         </ul>
     </div>
-
+    
     <nav>
+        <div class="nav-left">
+            <i class="fa-solid fa-circle-user" style="font-size: 36px"></i>
+            <i class="fa-regular fa-bell" style="font-size: 36px"></i>
+        </div>
+        <search>
+            <form action="" class="nav-search-bar" method="get">
+                <input type="text" placeholder="Cari konten atau user" name="search-account" class="nav-input-search">
+                <button type="submit" class="nav-search-button">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </form>
+        </search>
         <div class="sidebar-menu">
             <span></span>
             <span></span>
@@ -24,18 +37,33 @@
         </div>
     </nav>
     
-    <main class="main-container">
-        search bar123
-        <div class="list-music-container">
+    <main class="main-container" id="main">
+        <div class="main-content-container">
+            <div class="paragraph-content">
+                <p>Selamat Datang di HexaSync</p>
+                <p>Komunitas untuk berkarya</p>
+                <p></p>
+            </div> <br><br>
+            
+            <a href="" class="create-account-button">Buat Akun</a>
+        </div>
+        
+        
+        <div class="list-content-container">
             <?php
-                for ($i = 1; $i < 11; $i++){
+                for ($i = 1; $i < 6; $i++){
                     echo "
-                        <input type='radio' id='$i' name='lagu' value='$i'>
-                        <label for='$i'>
-                            <span class='list-music'>
-                                <script></script>
-                            </span>
-                        </label>
+                        <figure class='content-container'>
+                            <img src='assets/bag.png' alt='gambar-konten' class='thumbnail'>
+                            <figcaption class='caption-content'>
+                                <figure class='owner-content'>
+                                    <i class='fa-solid fa-circle-user' style='font-size: 36px'></i>
+                                    <figcaption class='owner-name'>aldi</figcaption>
+                                </figure>
+                                <p>Judul</p>
+                                <p>deskripsi</p>
+                            </figcaption>
+                        </figure>
                     ";
                 }
             ?>
