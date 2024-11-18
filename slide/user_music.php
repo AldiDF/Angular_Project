eebce29ca00affbe6d77696dff83e3e57377b91c
indioktaa <oktariaindi@gmail.com>
2024-11-18 22:10:50 +0800

    kelola user

--- slide/user_music.php
+++ slide/user_music.php
@@ -14,64 +14,44 @@
     <title>Kelola Musik</title>
     <link rel="stylesheet" href="../styles/main.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="../styles/transition.css?v=<?php echo time(); ?>">
-    <link rel="stylesheet" href="../styles/sidebar.css?v=<?php echo time(); ?>">
-    <link rel="stylesheet" href="../styles/navfooter.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="../styles/user.css?v=<?php echo time(); ?>"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer">
 </head>
 <body>
-    <?php include("../navfooter/sidebar.php")?>
-    <?php include("../navfooter/navbar.php")?>
-    <main>
+<main>
+    <div>
         <h1 class="heading-user-music">KELOLA LAGU</h1>
-        <search>
-            <form action="" class="search-bar-user" method="get">
-                <input type="text" placeholder="Cari Lagu" name="search-music" class="input-search-user">
-                <button type="submit" class="search-button-user">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </button>
-            </form>
-        </search>
-        <table border="1">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Tampilan</th>
-                    <th>Judul</th>
-                    <th>Deskripsi</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php $i = 1; foreach($lagu as $lagu): ?>
-                <?php $direktori = "../databases/thumbnail/" . $lagu["thumbnail"]; ?>
-                <tr>
-                    <td><?php echo $i; ?></td>
-                    <td><?php echo "<img src='$direktori' alt='thumbnail-picture' class='thumbnail-user'>"; ?></td>
-                    <td><?php echo $lagu["judul"]; ?></td>
-                    <td><?php echo $lagu["deskripsi"]; ?></td>
-                    <td>
-                        <div class="action-button">
-                            <a href="edit_lagu.php?lagu=<?php echo $lagu['lagu']; ?>">
-                                <button class="edit-icon">
-                                    <i class="fa-solid fa-pen-to-square"></i> Edit
-                                </button>
-                            </a>
-                            <a href="../databases/query.php?delete_lagu=true&session=user&lagu=<?php echo $lagu['lagu']?>" onclick="return confirm('Yakin ingin menghapus lagu ini?')">
-                                <button class="delete-icon">
-                                    <i class="fa-light fa-trash-can"></i> Hapus
-                                </button>
-                            </a>
-                        </div>
-                    </td>
-                </tr>
-                <?php $i++; endforeach; ?>
-            </tbody>
-        </table>
-    </main>
-    <?php include("../navfooter/footer.php")?>
-
-    <script src="../scripts/main.js?v=<?php echo time(); ?>"></script>
-    <script src="scripts/transition.js?v=<?php echo time(); ?>"></script>
+    </div>
+    
+    <div class="underline"></div>
+    <div class="search-bar-user">
+        <form action="" method="get">
+            <input type="text" placeholder="Cari Lagu" name="search-music" class="input-search-user">
+            <button type="submit" class="search-button-user">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+        </form>
+    </div>
+    <div class="music-list">
+        <?php foreach ($lagu as $lagu): ?>
+            <?php $direktori = "../databases/thumbnail/" . $lagu["thumbnail"]; ?>
+            <div class="music-card">
+                <img src="<?php echo $direktori; ?>" alt="Thumbnail Lagu" class="thumbnail-user">
+                <div class="music-info">
+                    <h3 class="music-title"><?php echo $lagu["judul"]; ?></h3>
+                    <p class="music-description"><?php echo $lagu["deskripsi"]; ?></p>
+                </div>
+                <div class="action-buttons">
+                    <a href="edit_lagu.php?lagu=<?php echo $lagu['lagu']; ?>" class="edit-button">
+                        <i class="fa-solid fa-pen-to-square"></i>
+                    </a>
+                    <a href="../databases/query.php?delete_lagu=true&session=user&lagu=<?php echo $lagu['lagu']; ?>" class="delete-button" onclick="return confirm('Yakin ingin menghapus lagu ini?')">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </a>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</main>
 </body>
 </html>
