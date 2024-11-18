4c5c1f9e2defa30da711d1ba8a8988d77078e51d
AldiDF <aldidaffaarisyi@gmail.com>
2024-11-18 21:41:04 +0800

    revisi

--- index.php
+++ index.php
@@ -21,6 +21,8 @@
     <link rel="stylesheet" href="styles/sidebar.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="styles/navfooter.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="styles/login_signup.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="styles/edit.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="styles/user.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css">
     <style>
