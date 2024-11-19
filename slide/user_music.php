b30243ecfbf20e1a8ab6a70bbe49f353080d0e95
indioktaa <oktariaindi@gmail.com>
2024-11-19 17:09:41 +0800

    user

--- slide/user_music.php
+++ slide/user_music.php
@@ -1,53 +1,53 @@
 <?php
-session_start();
-require "../databases/connection.php";
-$query = "SELECT * FROM content";
-$result = $conn->query($query);
+    require "../databases/connection.php";
+    include "../databases/query.php";
 
-$lagu = $result->fetch_all(MYSQLI_ASSOC);
+    $lagu = select_lagu($conn, "ACCEPT", "");
 ?>
 
-<div id="userMusic" class="user-musicpg" style="display: none;">
-    <div class="title-upper">
-        <button class="back-page" onclick="closeMusic()">
-            <i class="fa-solid fa-arrow-left" style="font-size: 30px"></i>
-        </button>
-        <h1>KELOLA LAGU</h1>
-    </div>
-    <div class="music-list">
-        <?php if (!empty($lagu)): ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kelola Musik</title>
+    <link rel="stylesheet" href="../styles/main.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../styles/user.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer">
+</head>
+<body>
+    <main>
+        <h1 class="heading-user-music">KELOLA LAGU</h1>
+        <div class="underline"></div> <!-- Garis bawah sesuai kelas .underline -->
+        
+        <!-- Search Bar -->
+        <search>
+            <form action="" class="search-bar-user" method="get">
+                <input type="text" placeholder="Cari Lagu" name="search-music" class="input-search-user">
+                <button type="submit" class="search-button-user">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </form>
+        </search>
+
+        <!-- Music List -->
+        <div class="music-list">
             <?php foreach ($lagu as $lagu): ?>
-                <?php $direktori = "databases/thumbnail/" . $lagu["thumbnail"]; ?>
-                <div class="music-card">
-                    <img src="<?php echo $direktori; ?>" alt="Thumbnail Lagu" class="thumbnail-user">
-                    <div class="music-info">
-                        <h3 class="music-title"><?php echo $lagu["judul"]; ?></h3>
-                        <p class="music-description"><?php echo $lagu["deskripsi"]; ?></p>
-                    </div>
-                    <div class="action-buttons">
-                        <a href="edit_lagu.php?lagu=<?php echo $lagu['lagu']; ?>" class="edit-button">
-                            <i class="fa-solid fa-pen-to-square"></i>
-                        </a>
-                        <a href="databases/query.php?delete_lagu=true&session=user&lagu=<?php echo $lagu['lagu']; ?>" 
-                           class="delete-button" 
-                           onclick="return confirm('Yakin ingin menghapus lagu ini?')">
-                            <i class="fa-solid fa-trash-can"></i>
-                        </a>
-                    </div>
+            <?php $thumbnailPath = "../databases/thumbnail/" . $lagu["thumbnail"]; ?>
+            <div class="music-card">
+                <img src="<?php echo $thumbnailPath; ?>" alt="Thumbnail" class="thumbnail-user">
+                <div class="music-info">
+                    <h2 class="music-title"><?php echo $lagu["judul"]; ?></h2>
+                    <p class="music-description"><?php echo $lagu["deskripsi"]; ?></p>
                 </div>
+                <div class="action-buttons">
+                    <a href="edit_lagu.php?lagu=<?php echo $lagu['lagu']; ?>" class="edit-button">Edit</a>
+                    <a href="../databases/query.php?delete_lagu=true&session=user&lagu=<?php echo $lagu['lagu']; ?>" 
+                       onclick="return confirm('Yakin ingin menghapus lagu ini?')" class="delete-button">Hapus</a>
+                </div>
+            </div>
             <?php endforeach; ?>
-        <?php else: ?>
-            <p>Tidak ada lagu ditemukan.</p>
-        <?php endif; ?>
-    </div>
-</div>
-
-<script>
-function closeMusic() {
-    document.getElementById('userMusic').style.display = 'none';
-}
-
-function showMusic() {
-    document.getElementById('userMusic').style.display = 'block';
-}
-</script>
+        </div>
+    </main>
+</body>
+</html>
