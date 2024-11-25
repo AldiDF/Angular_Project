6fe7d11b12d1ef121471cf7d9870d9a16aab6d54
Andhika <18984@sman1samarinda.sch.id>
2024-11-25 22:39:46 +0800

    minimal 8 karakter

--- slide/signup.php
+++ slide/signup.php
@@ -19,7 +19,7 @@
                 <label for="username">Nama Pengguna:</label>
                 <input type="text" id="username" name="username" class="form-login" required><br>
                 <label for="password">Sandi:</label>
-                <input type="password" id="password" name="password" class="form-login" required><br>
+                <input type="password" id="password" name="password" class="form-login" minlength="5" required><br>
                 <div class="submit-button-container">
                     <input type="submit" id="submit-login" name="signup" value="Daftar" class="submit-button-signup">
                 </div><br>
